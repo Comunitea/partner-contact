7dd0cfbd636ca725be865ff4eeffecfee82a61e5
javierjcf <javierjcf@gmail.com>
2018-10-01 18:52:10 +0200

    [MIG]partner_risk_insurance: Migrado a v11

--- partner_risk_insurance/__manifest__.py
+++ partner_risk_insurance/__manifest__.py
@@ -6,7 +6,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Partner Risk Insurance',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Sales Management',
     'summary': 'Lets set risk insurance info in the partners',
     'author': 'AvanzOSC,'
@@ -22,5 +22,5 @@
     'data': [
         'views/res_partner_view.xml',
     ],
-    'installable': False,
+    'installable': True,
 }
